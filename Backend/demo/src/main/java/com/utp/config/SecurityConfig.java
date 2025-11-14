1b7d9ee2e8abb671c26b3cb5064fd06574ecaefb
Thiago <U22330428@utp.edu.pe>
2025-11-13 22:28:07 -0500

    Incorporando cambios para probar backend con seguridad hasheo de password y proteccion de endpoints 4

--- Backend/demo/src/main/java/com/utp/config/SecurityConfig.java
+++ Backend/demo/src/main/java/com/utp/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -19,6 +20,14 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.configurationSource(request -> {
+                    var config = new org.springframework.web.cors.CorsConfiguration();
+                    config.setAllowedOrigins(List.of("*"));
+                    config.setAllowedMethods(List.of("*"));
+                    config.setAllowedHeaders(List.of("*"));
+                    config.setAllowCredentials(false);
+                    return config;
+                }))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/api/personal/**","/api/personal").hasAuthority("Administrador de Sistemas")
